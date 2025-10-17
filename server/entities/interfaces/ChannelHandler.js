50726597d366b59b6cfa79d921b40b4411d88bc7
Jhon Wilker <jhonwilker0112@gmail.com>
2025-10-16 22:00:09 -0300

    add: classe interface para tratamento de contextos com clientes

---
+++ server/entities/interfaces/ChannelHandler.js
@@ -0,0 +1,70 @@
+import { User } from "../User.js"
+
+export class ChannelHandler {
+    #clientsIdAccummulator = 0
+
+    constructor(commPort) {
+        this.port = commPort
+        this.users = new Map()
+        this.commands = {}
+    }
+
+    addUser(port) {
+        let user = new User(this.#clientsIdAccummulator, '', port)
+
+        user.port.on('message', ({ command, payload }) => {
+            try {
+                if (!payload) {
+                    payload = {}
+                }
+
+                payload['user'] = user
+                this.commands[command](payload)
+            }
+            catch (err) {
+                if (err instanceof TypeError) {
+                    console.log(err)
+                    user.respond('error', { msg: 'Comando não reconhecido' })
+                }
+                else {
+                    console.log('ERRO INESPERADO:', err.message)
+                    user.respond('error', { msg: 'Erro inesperado no servidor' })
+                }
+            }
+        })
+
+        this.users.set(this.#clientsIdAccummulator, user)
+        this.#clientsIdAccummulator++
+
+        return user
+    }
+
+    listUsers() {
+        return [...this.users.values()].map(user => user.nickname)
+    }
+
+    removeUser(id) {
+        return this.users.delete(id)
+    }
+
+    changeUserNickname(user, newNickname) {
+        const oldNick = user.nickname
+        this.users.get(user.id).nickname = newNickname
+
+        user.respond('success', {
+            oldNick,
+            newNick: newNickname
+        })
+    }
+
+    broadcastMessage(sender, message) {
+        this.users.values().forEach(user => {
+            if (user.id === sender.id) return
+
+            user.respond('broadcast', {
+                sender: sender.nickname,
+                message
+            })
+        });
+    }
+}
